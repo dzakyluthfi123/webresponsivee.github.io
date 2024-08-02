148f2473ab8d49083860c9663aa6615e532e40ff
dzakyluthfi123 <dzakylutfi22@gmail.com>
2024-08-02 18:20:03 +0700

    memilah posisi dari masing masing index

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+    <div class="social">
+        <a href="https://www.instagram.com/m45j4i/"><i class='bx bxl-instagram'></i></a>
+        <a href="#"><i class='bx bxl-whatsapp'></i></a>
+        <a href="https://www.facebook.com/profile.php?id=100050076496327"><i class='bx bxl-facebook'></i></a>
+    </div>
+
+    <ul class="list">
+        <li>
+            <a href="#">FAQ</a>
+        </li>
+        <li>
+            <a href="#">Services</a>
+        </li>
+        <li>
+            <a href="#">About Me</a>
+        </li>
+        <li>
+            <a href="#">Contact</a>
+        </li>
+        <li>
+            <a href="#">Privacy Policy</a>
+        </li>
+    </ul>
+    <p class="copyright">
+        @Masjai | All Right Reserved
+    </p>
+</footer>
+
+<script src="script.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+include 'header.php';
+?>
+<section class="home" id="home">
+    <div class="home-img">
+        <img src="FOTO_SAYA-Copy1.jpg" alt="">
+    </div>
+
+    <div class="home-content">
+        <h1>It's <span>DzakyLuthfi</span></h1>
+        <h3 class="text-animation">
+            I'm a <span></span>
+        </h3>
+        <h1>YUHLEZ Software House</h1><br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium eligendi deserunt veniam modi eum
+            maxime
+            est eius, ea tenetur quos possimus,
+            quaerat incidunt excepturi. Repellat nobis sunt voluptas nam nulla.</p>
+
+        <div class="social-icons">
+            <a href="https://www.instagram.com/m45j4i/"><i class='bx bxl-instagram'></i></a>
+            <a href="#"><i class='bx bxl-whatsapp'></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100050076496327"><i class='bx bxl-facebook'></i></a>
+        </div>
+
+        <a href="#" class="btn">Hire Me</a>
+    </div>
+</section>
+
+<section class="about" id="about">
+    <div class="about-content">
+        <h2 class="heading">About <span>Me</span></h2>
+        <h3 class="text-animation"> <span></span></h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem magni doloremque error animi est
+            doloribus itaque dolor molestias,
+            cumque laboriosam aliquam deserunt distinctio possimus atque, nihil fugit numquam. Commodi, doloribus!
+        </p>
+        <a href="#" class="btn">Read More</a>
+    </div>
+    <div class="about-img">
+        <img src="FOTO_SAYA-Copy1.jpg" alt="">
+    </div>
+</section>
+
+<section class="services" id="services">
+    <h2 class="heading"> Services</h2>
+
+    <div class="services-container">
+        <div class="services-box">
+            <div class="services-info">
+                <h4>Graphic Desain</h4>
+                <p>lorem</p>
+            </div>
+        </div>
+        <div class="services-box">
+            <div class="services-info">
+                <h4>Frontend Development</h4>
+                <p>lorem</p>
+            </div>
+        </div>
+        <div class="services-box">
+            <div class="services-info">
+                <h4>Graphic Desain</h4>
+                <p>lorem</p>
+            </div>
+        </div>
+        <div class="services-box">
+            <div class="services-info">
+                <h4>Graphic Desain</h4>
+                <p>lorem</p>
+            </div>
+        </div>
+        <div class="services-box">
+            <div class="services-info">
+                <h4>Graphic Desain</h4>
+                <p>lorem</p>
+            </div>
+        </div>
+        <div class="services-box">
+            <div class="services-info">
+                <h4>Graphic Desain</h4>
+                <p>lorem</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="contact" id="contact">
+    <h2 class="heading">Contact <span>Me</span></h2>
+
+    <form action="#">
+        <div class="input-box">
+            <input type="text" placeholder="Full Name">
+            <input type="email" placeholder="Email">
+        </div>
+        <div class="input-box">
+            <input type="number" placeholder="Phone Number">
+            <input type="text" placeholder="Subject">
+        </div>
+        <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+
+        <input type="submit" value="Send Message" class="btn">
+    </form>
+</section>
+
+
+
+<!-- <script src="script.js"></script> -->
+
+<?php
+include 'footer.php';
+?>
