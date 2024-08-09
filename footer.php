95f018df029cee3e391cd31857b94f4bb834257e
dzakyluthfi123 <dzakylutfi22@gmail.com>
2024-08-09 13:34:02 +0700

    menambahkan ikon

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer class="footer">
     <div class="social">
-        <a href="https://www.instagram.com/m45j4i/"><i class='bx bxl-instagram'></i></a>
-        <a href="#"><i class='bx bxl-whatsapp'></i></a>
-        <a href="https://www.facebook.com/profile.php?id=100050076496327"><i class='bx bxl-facebook'></i></a>
+        <a href="https://www.instagram.com/m45j4i/"><i class="bi bi-instagram"></i>
+            <a href="#"><i class="bi bi-whatsapp"></i>
+                <a href="https://www.facebook.com/profile.php?id=100050076496327"><i class="bi bi-facebook"></i>
     </div>
 
     <ul class="list">
